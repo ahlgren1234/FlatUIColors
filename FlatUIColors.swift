e1dc200d3a45ea8b2c1fe7be4359922718665760
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-17 16:36:45 +0100

    Added da file.

---
+++ FlatUIColors.swift
@@ -0,0 +1,49 @@
+//
+//  FlatUIColors.swift
+//  flatuicolors
+//
+//  Created by Peter Ahlgren on 2017-12-17.
+//  Copyright © 2017 Peter Ahlgren. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Flat UI Colors from https://flatuicolors.com
+
+// Green colors
+let turquoise = UIColor(red:0.10, green:0.74, blue:0.61, alpha:1.0)
+let emerald = UIColor(red:0.18, green:0.80, blue:0.44, alpha:1.0)
+let greenSea = UIColor(red:0.09, green:0.63, blue:0.52, alpha:1.0)
+let nephritis = UIColor(red:0.15, green:0.68, blue:0.38, alpha:1.0)
+
+// Blue colors
+let peterRiver = UIColor(red:0.20, green:0.60, blue:0.86, alpha:1.0)
+let belizeHole = UIColor(red:0.16, green:0.50, blue:0.73, alpha:1.0)
+
+// Purple colors
+let amethyst = UIColor(red:0.61, green:0.35, blue:0.71, alpha:1.0)
+let wisteria = UIColor(red:0.56, green:0.27, blue:0.68, alpha:1.0)
+
+// Yellow
+let sunflower = UIColor(red:0.95, green:0.77, blue:0.06, alpha:1.0)
+
+// Orange colors
+let orange = UIColor(red:0.95, green:0.61, blue:0.07, alpha:1.0)
+let carrot = UIColor(red:0.90, green:0.49, blue:0.13, alpha:1.0)
+let pumpkin = UIColor(red:0.83, green:0.33, blue:0.00, alpha:1.0)
+
+// Red colors
+let alizarin = UIColor(red:0.91, green:0.30, blue:0.24, alpha:1.0)
+let pomegranate = UIColor(red:0.75, green:0.22, blue:0.17, alpha:1.0)
+
+// Gray colors
+let clouds = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.0)
+let silver = UIColor(red:0.74, green:0.76, blue:0.78, alpha:1.0)
+let concrete = UIColor(red:0.58, green:0.65, blue:0.65, alpha:1.0)
+let asbestos = UIColor(red:0.50, green:0.55, blue:0.55, alpha:1.0)
+
+// Dark colors
+let wetAsphalt = UIColor(red:0.20, green:0.29, blue:0.37, alpha:1.0)
+let MidnightBlue = UIColor(red:0.17, green:0.24, blue:0.31, alpha:1.0)
+
